5066b16d874eafc731c98a08a1a882ca45324372
KentFujii <kent.where.the.light.is@gmail.com>
2017-12-17 15:58:40 +0900

    finish: kernel_pca

--- dimensionality_reduction/rbf_kernel_pca.py
+++ dimensionality_reduction/rbf_kernel_pca.py
@@ -5,13 +5,19 @@ import numpy as np
 
 
 def rbf_kernel_pca(X, gamma, n_components):
+    # それぞれ行すべての組み合わせの距離を求める
     sq_dists = pdist(X, 'sqeuclidean')
+    # 上で求めたそれぞれの組み合わせの距離を行列で表現する
     mat_sq_dists = squareform(sq_dists)
+    # rbf対称カーネル行列を計算する
     K = exp(-gamma * mat_sq_dists)
     N = K.shape[0]
     one_n = np.ones((N, N)) / N
+    # カーネル行列Kの中心化を行う
     K = K - one_n.dot(K) - K.dot(one_n) + one_n.dot(K).dot(one_n)
     eigvals, eigvecs = eigh(K)
+    # n_componentsの数だけ固有値を抽出する
     alphas = np.column_stack((eigvecs[:, -i] for i in range(1, n_components + 1)))
+    # n_componentsの数だけ固有ベクトルを抽出する
     lambdas = [eigvals[-i] for i in range(1, n_components + 1)]
     return alphas, lambdas
